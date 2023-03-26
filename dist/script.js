18e799827a96e1b034240476f521ed63eeb81664
Sergey <flargoin@gmail.com>
2023-03-26 08:01:13 +0300

    mod effects

--- dist/script.js
+++ dist/script.js
@@ -485,6 +485,30 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.fadeOut = function (dur,
   }
   return this;
 };
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.fadeToggle = function (dur) {
+  let display = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'block';
+  let fin = arguments.length > 2 ? arguments[2] : undefined;
+  for (let i = 0; i < this.length; i++) {
+    if (window.getComputedStyle(this[i]).display === 'none') {
+      this[i].style.display = display;
+      const _fadeIn = complection => {
+        this[i].style.opacity = complection;
+      };
+      const ani = this.animateOverTime(dur, _fadeIn, fin);
+      requestAnimationFrame(ani);
+    } else {
+      const _fadeOut = complection => {
+        this[i].style.opacity = 1 - complection;
+        if (complection === 1) {
+          this[i].style.display = 'none';
+        }
+      };
+      const ani = this.animateOverTime(dur, _fadeOut, fin);
+      requestAnimationFrame(ani);
+    }
+  }
+  return this;
+};
 
 /***/ }),
 
@@ -542,13 +566,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _lib_lib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./lib/lib */ "./src/js/lib/lib.js");
 
 $('#first').click(function () {
-  $('div').eq(1).fadeOut(800);
+  $('div').eq(1).fadeToggle(800);
 });
 $('[data-count="second"]').click(function () {
-  $('div').eq(2).fadeOut(800);
+  $('div').eq(2).fadeToggle(800);
 });
 $('button').eq(2).click(function () {
-  $('.w-500').fadeOut(800);
+  $('.w-500').fadeToggle(800);
 });
 
 /***/ })
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
 import './lib/lib';
 
 $('#first').click(function() {
-    $('div').eq(1).fadeOut(800);
+    $('div').eq(1).fadeToggle(800);
 });
 
 $('[data-count="second"]').click(function() {
-    $('div').eq(2).fadeOut(800);
+    $('div').eq(2).fadeToggle(800);
 });
 
 $('button').eq(2).click(function() {
-    $('.w-500').fadeOut(800);
+    $('.w-500').fadeToggle(800);
 });
